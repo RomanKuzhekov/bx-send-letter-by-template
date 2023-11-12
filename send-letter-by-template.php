4106d18e4c0852a14b291041550fb8f8e5c94d4d
Roman <profet@inbox.ru>
2023-11-12 22:37:38 +0300

    Файл для подключения компонента

---
+++ send-letter-by-template.php
@@ -0,0 +1,6 @@
+<? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Отправка письма по шаблону"); ?>
+
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
