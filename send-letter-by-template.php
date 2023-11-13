f7c8f33a842a2feab1c634496d8b46543f6f697d
Roman <profet@inbox.ru>
2023-11-13 15:57:30 +0300

    Подключили компонент

--- send-letter-by-template.php
+++ send-letter-by-template.php
@@ -1,6 +1,14 @@
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Отправка письма по шаблону"); ?>
-
-
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$APPLICATION->SetTitle("Отправка письма по шаблону"); ?><?$APPLICATION->IncludeComponent(
+	"send.letter.by.template",
+	"",
+	Array(
+		"IBLOCK" => "6",
+		"MAIL_FROM" => "Рассылка писем",
+		"MAIL_TEXT" => "Добрый день. Прикладываю файл.",
+		"MAIL_TITLE" => "Письмо по шаблону",
+		"PAGER_NUMBER" => "10",
+		"PL_EMAIL" => "#email#",
+		"PL_FIO" => "#ФИО#"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
